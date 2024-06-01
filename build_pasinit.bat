f10d80d0077a26d03b6053c76dfcb3ef8f603390
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2024-06-01 11:15:10 -0400

    Updated to builds without circular dependencies.

--- build_pasinit.bat
+++ build_pasinit.bat
@@ -4,11 +4,6 @@ rem   Set up for building a Pascal module.
 rem
 call build_vars
 
-call src_go %srcdir%
-call src_getfrom sys base.ins.pas
-call src_getfrom sys sys.ins.pas
-call src_getfrom util util.ins.pas
-call src_getfrom string string.ins.pas
-call src_getfrom file file.ins.pas
+call src_getbase
 
 call src_builddate "%srcdir%"
